4291abf31f337f3dfec0bdfed3abe6867d7950d5
greysome <wayyan.win@gmail.com>
2023-12-25 18:35:57 +0800

    document freq.c/.h
    I also renamed some stuff and simplified some of the logic.

--- freq.h
+++ freq.h
@@ -2,23 +2,28 @@
 #define FREQ
 
 #include "raylib.h"
-#include "globals.h"
-#include "debug.h"
 #include "note.h"
 #include "util.h"
 
-// Moving up a semitone increases the Hz by this factor
-#define SEMITONE 1.05946
-// In Hz
-#define C4 261.6
+#define SEMITONE 1.05946 // Moving up a semitone scales the frequency by this factor
+#define C4 261.6 // In Hz
 
-float get_note_freq(int note, int octave);
+// Pitch modifiers refer to the following expressive controls:
+// pitch bend, gliss, dive, vibrato, autogliss;
+// which can alter the pitch of the currently played note.
+
+float get_note_freq(int note, int octave); /* Get frequency of note at octave, without any pitch modifiers. */
+ /* Get frequency of note at octave, with pitch modifiers.
+    NOTE: this does not account for autogliss. */
 float get_actual_freq(int note, int octave);
 void update_pitch_bend();
 void update_gliss();
-void update_effects();
+void update_dive();
 void update_vib();
 void update_autogliss();
+/* Returns a table of frequencies for each note in the notetable, accounting for pitch modifiers.
+   Unlike get_actual_freq(), this accounts for autogliss.
+   NOTE: ths can be used in solo mode, but you should only trust the entry given by get_cur_note(). */
 float *get_cur_actual_freqs();
 void reset_freq_modifiers();
 
